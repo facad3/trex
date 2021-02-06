2ee6ff3b19b30aacf36c7a86f95092ceca8c1321
facad3 <53523282+facad3@users.noreply.github.com>
2021-02-06 17:51:03 -0500

    Update ~LEGACY~START.bat

--- ~LEGACY~START.bat
+++ ~LEGACY~START.bat
@@ -1,1 +1,1 @@
-t-rex -a ethash -o stratum+tcp://us1.ethermine.org:4444 -u 0xF832bcD68e1943098A41F6D3c0073012E2018733 -p x
+t-rex -a ethash -o stratum+tcp://us1.ethermine.org:4444 -u 0xF832bcD68e1943098A41F6D3c0073012E2018733.laptop -p x
